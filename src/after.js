636183cb66a2d710a282eeb5dc58036609b6f646
backpacker69 <backpacker69@protonmail.com>
2019-07-04 15:31:00 +0000

    add log and createHash

--- src/after.js
+++ src/after.js
@@ -1,2 +1,5 @@
 window.Btc = NewLedger.Btc.default;
 window.TransportWebUSB = NewLedger.TransportWebUSB.default;
+window.TransportU2F = NewLedger.TransportU2F.default;
+window.Log = NewLedger.Log.default;
+window.createHash = NewLedger.createHash.default;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import regeneratorRuntime from "regenerator-runtime";
 import * as Btc from "@backpacker69/hw-app-btc";
 import * as TransportWebUSB from "@backpacker69/hw-transport-webusb";
-
+import * as TransportU2F from "@ledgerhq/hw-transport-u2f";
+import * as Log from "@ledgerhq/logs";
+import * as createHash from "create-hash";
 export {
   Btc,
-  TransportWebUSB
+  TransportWebUSB,
+  TransportU2F,
+  Log,
+  createHash
 }
