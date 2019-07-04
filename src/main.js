a253aa61044be10bbdadd6dc03710fc205ef7500
backpacker69 <backpacker69@protonmail.com>
2019-07-04 16:25:32 +0000

    browser version of create hash is what we need

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import * as Btc from "@backpacker69/hw-app-btc";
 import * as TransportWebUSB from "@backpacker69/hw-transport-webusb";
 import * as TransportU2F from "@ledgerhq/hw-transport-u2f";
 import * as Log from "@ledgerhq/logs";
-import * as createHash from "create-hash";
+import * as createHash from "create-hash/browser";
 export {
   Btc,
   TransportWebUSB,
